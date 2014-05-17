5c0d40a93d7f5b6f331f899e3cc470db237a5d2c
def <dennis@felsin9.de>
2014-05-17 18:57:46 +0200

    Only consider dummy connected when 2 snapshots were received
    Slightly modified by @ChillerDragon to work with ZillyWoods
    (cherry picked from commit https://github.com/ddnet/ddnet/commit/2c1dc335cc0a8baa900b256732eebee033565134)
    (cherry picked from commit f5c6236cbe7cd71ebe5f036a7bcf593b01482e71)

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -1691,15 +1691,7 @@ void CClient::ProcessServerPacketDummy(CNetChunk *pPacket)
 
 	if(Sys)
 	{
-		if(Msg == NETMSG_CON_READY)
-		{
-			//GameClient()->OnConnected();
-			m_DummyConnected = true;
-			m_LastDummyConnectTime = GameTick();
-			g_Config.m_ClDummy = 1;
-			GameClient()->SwitchDummy();
-		}
-		else if(Msg == NETMSG_SNAP || Msg == NETMSG_SNAPSINGLE || Msg == NETMSG_SNAPEMPTY)
+		if(Msg == NETMSG_SNAP || Msg == NETMSG_SNAPSINGLE || Msg == NETMSG_SNAPEMPTY)
 		{
 			int NumParts = 1;
 			int Part = 0;
@@ -1873,6 +1865,12 @@ void CClient::ProcessServerPacketDummy(CNetChunk *pPacket)
 						m_aSnapshots[!g_Config.m_ClDummy][SNAP_CURRENT] = m_SnapshotStorage[!g_Config.m_ClDummy].m_pLast;
 						m_LocalStartTime = time_get();
 						SetState(IClient::STATE_ONLINE);
+
+						m_DummyConnected = true;
+						m_LastDummyConnectTime = GameTick;
+						g_Config.m_ClDummy = 1;
+						GameClient()->SwitchDummy();
+
 						//DemoRecorder_HandleAutoStart();
 					}
 
