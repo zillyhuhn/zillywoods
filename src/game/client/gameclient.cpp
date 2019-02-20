becae7ef2896e521efbc4db504f9f7d9237793b8
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2019-02-20 14:20:58 +0100

    Implement cl_show_silent_messages
    (cherry picked from commit https://github.com/Dune-jr/teeworlds/commit/1195c7f81ec7d3cf9fd1f01b83b2ff70d0ff8cd2)
    (cherry picked from commit 5eb9e427d5bc37d5cb38014c8bf0a67c8ab1e2c5)

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -856,7 +856,7 @@ void CGameClient::OnMessage(int MsgId, CUnpacker *pUnpacker, bool IsDummy)
 				return;
 			}
 
-			if(m_LocalClientID != -1 && !pMsg->m_Silent)
+			if(m_LocalClientID != -1 && !(pMsg->m_Silent && !g_Config.m_ClShowSilentMessages))
 			{
 				DoEnterMessage(pMsg->m_pName, pMsg->m_ClientID, pMsg->m_Team);
 
@@ -917,7 +917,7 @@ void CGameClient::OnMessage(int MsgId, CUnpacker *pUnpacker, bool IsDummy)
 			return;
 		}
 
-		if(!pMsg->m_Silent)
+		if(!(pMsg->m_Silent && !g_Config.m_ClShowSilentMessages))
 		{
 			DoLeaveMessage(m_aClients[pMsg->m_ClientID].m_aName, pMsg->m_ClientID, pMsg->m_pReason);
 
@@ -1005,7 +1005,7 @@ void CGameClient::OnMessage(int MsgId, CUnpacker *pUnpacker, bool IsDummy)
 			}
 		}
 
-		if(pMsg->m_Silent == 0)
+		if(!(pMsg->m_Silent && !g_Config.m_ClShowSilentMessages))
 		{
 			DoTeamChangeMessage(m_aClients[pMsg->m_ClientID].m_aName, pMsg->m_ClientID, pMsg->m_Team);
 		}
