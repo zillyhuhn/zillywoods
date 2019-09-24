d94b14e0a02359983d396a2f8e5d8bd6ee447742
ChillerDragon <ChillerDragon@gmail.com>
2019-09-24 19:50:20 +0200

    Try to be as flexible/supportive and future proof
    Send also timeout rcon command in addition to ZillyWoods messages.
    In case the old ddnet system will be reintroduced it will me a chat command and thus also a rcon command.
    So the server code can be cleaner just supporting the rcon. Which is better than the chat system anyways.

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -1358,6 +1358,8 @@ void CGameClient::SendTimeoutCode()
 	char aBuf[128];
 	str_format(aBuf, sizeof(aBuf), "ZillyWoods (v%s) code=%s", ZILLYWOODS_VERSION, g_Config.m_ClTimeoutCode);
 	m_pClient->Rcon(aBuf);
+	str_format(aBuf, sizeof(aBuf), "timeout %s", g_Config.m_ClTimeoutCode);
+	m_pClient->Rcon(aBuf);
 	m_SentTimeoutCode = true;
 }
 
