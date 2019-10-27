976e03bbaf20d8631f13195dce80263c2af3ec19
ChillerDragon <ChillerDragon@gmail.com>
2019-10-27 18:50:41 +0100

    Fix selected tee on dummy enter
    Thanks to @def-
    Inspired by https://github.com/ddnet/ddnet/commit/4782d346673b31cae9dc38bb0e2b15bfbbfb9253
    
    (cherry picked from commit 2e83843076e47c0150c40f8e9ab4f252cdeffce6)

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -277,6 +277,7 @@ public:
 	// TODO: move these
 	void SendSwitchTeam(int Team);
 	virtual void SendStartInfo(bool IsDummy = false);
+	virtual void SwitchDummy();
 	void SendKill();
 	void SendReadyChange();
 	void SendSkinChange();
