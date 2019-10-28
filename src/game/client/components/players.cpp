nonosch07/Blockworlds

Feature: Separate inputs for dummy and player

src/game/client/components/players.cpp
@@ -410,7 +410,7 @@ void CPlayers::RenderPlayer(
 	if(pInfo.m_Local && Client()->State() != IClient::STATE_DEMOPLAYBACK)
 	{
 		// just use the direct input if it's local player we are rendering
-		Angle = GetAngle(m_pClient->m_pControls->m_MousePos);
+		Angle = GetAngle(m_pClient->m_pControls->m_MousePos[g_Config.m_ClDummy]);
 	}
 	else
 	{
@@ -544,7 +544,7 @@ void CPlayers::RenderPlayer(
 			vec2 ExDirection = Direction;
 
 			if (pPlayerInfo->m_Local && Client()->State() != IClient::STATE_DEMOPLAYBACK)
-				ExDirection = normalize(vec2(m_pClient->m_pControls->m_InputData.m_TargetX, m_pClient->m_pControls->m_InputData.m_TargetY));
+				ExDirection = normalize(vec2(m_pClient->m_pControls->m_InputData[g_Config.m_ClDummy].m_TargetX, m_pClient->m_pControls->m_InputData[g_Config.m_ClDummy].m_TargetY));
 
 			Graphics()->TextureSet(-1);
 			vec2 initPos = Position;
@@ -878,7 +878,7 @@ void CPlayers::RenderPlayer(
 		{
 			float a = 1;
 			if(g_Config.m_ClNameplatesAlways == 0)
-				a = clamp(1-powf(distance(m_pClient->m_pControls->m_TargetPos, Position)/200.0f,16.0f), 0.0f, 1.0f);
+				a = clamp(1-powf(distance(m_pClient->m_pControls->m_TargetPos[g_Config.m_ClDummy], Position)/200.0f,16.0f), 0.0f, 1.0f);
 			
 			const char *pName = m_pClient->m_aClients[pPlayerInfo->m_ClientID].m_aName;
 			float tw = TextRender()->TextWidth(0, FontSize, pName, -1);

zillyhuhn/zillywoods

Tweaked by @ChillerDragon
(cherry picked from commit https://github.com/ddnet/ddnet/commit/5b1c866b357e913ce8960f1e1203abee0016c566)
(cherry picked from commit 93496bb09738b4053e2dc64a2b77ab4a9248354d)

src/game/client/components/players.cpp
@@ -170,7 +170,7 @@ void CPlayers::RenderPlayer(
 	if(m_pClient->m_LocalClientID == ClientID && Client()->State() != IClient::STATE_DEMOPLAYBACK)
 	{
 		// just use the direct input if it's local player we are rendering
-		Angle = angle(m_pClient->m_pControls->m_MousePos);
+		Angle = angle(m_pClient->m_pControls->m_MousePos[g_Config.m_ClDummy]);
 	}
 	else
 	{
@@ -272,7 +272,7 @@ void CPlayers::RenderPlayer(
 
 	// draw gun
 	{
-		if(m_pClient->m_pControls->m_ShowHookColl && m_pClient->m_LocalClientID == ClientID)
+		if(m_pClient->m_pControls->m_ShowHookColl[g_Config.m_ClDummy] && m_pClient->m_LocalClientID == ClientID)
 		{
 			Graphics()->TextureClear();
 			vec2 initPos = Position + Direction * 42.0f;