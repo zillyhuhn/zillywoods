nonosch07/Blockworlds

Fix: Proper scoreboard font size

src/game/client/components/scoreboard.cpp
@@ -210,7 +210,7 @@ void CScoreboard::RenderScoreboard(float x, float y, float w, int Team, const ch
 	float LineHeight = 60.0f;
 	float TeeSizeMod = 1.0f;
 	float Spacing = 16.0f;
-	if(m_pClient->m_Snap.m_aTeamSize[Team] > 16)
+	if(m_pClient->m_Snap.m_aTeamSize[Team] > 32)
 	{
 		LineHeight = 20.0f;
 		TeeSizeMod = 0.4f;
@@ -254,7 +254,7 @@ void CScoreboard::RenderScoreboard(float x, float y, float w, int Team, const ch
 	// render player entries
 	y += HeadlineFontsize*2.0f;
 	float FontSize = 24.0f;
-	if(m_pClient->m_Snap.m_aTeamSize[Team] > 16)
+	if(m_pClient->m_Snap.m_aTeamSize[Team] > 32)
 		FontSize = 16.0f;
 	CTextCursor Cursor;
 
@@ -278,7 +278,7 @@ void CScoreboard::RenderScoreboard(float x, float y, float w, int Team, const ch
 			Graphics()->TextureSet(-1);
 			Graphics()->QuadsBegin();
 			Graphics()->SetColor(1.0f, 1.0f, 1.0f, 0.25f);
-			if(m_pClient->m_Snap.m_aTeamSize[Team] > 16)
+			if(m_pClient->m_Snap.m_aTeamSize[Team] > 32)
 				RenderTools()->DrawRoundRect(x, y, w-20.0f, LineHeight, 5.0f);
 			else
 				RenderTools()->DrawRoundRect(x, y, w-20.0f, LineHeight, 15.0f);

zillyhuhn/zillywoods

(cherry picked from commit https://github.com/ddnet/ddnet/commit/44964130c359ce376dd46f9b6d1eb904d56595c1)

src/game/client/components/scoreboard.cpp
@@ -214,7 +214,7 @@ float CScoreboard::RenderScoreboard(float x, float y, float w, int Team, const c
 
 	float HeadlineHeight = 40.0f;
 	float TitleFontsize = 20.0f;
-	float HeadlineFontsize = m_pClient->m_GameInfo.m_aTeamSize[Team] > 16 ? 8.0f : 12.0f;
+	float HeadlineFontsize = m_pClient->m_GameInfo.m_aTeamSize[Team] > 32 ? 8.0f : 12.0f;
 	float LineHeight = 20.0f;
 	float TeeSizeMod = 1.0f;
 	float Spacing = 2.0f;
@@ -241,7 +241,7 @@ float CScoreboard::RenderScoreboard(float x, float y, float w, int Team, const c
 		ScoreOffset = DeathOffset+DeathLength, ScoreLength = Race ? 83.0f : 35.0f;
 	}
 	float tw = 0.0f;
-	if(m_pClient->m_GameInfo.m_aTeamSize[Team] > 16)
+	if(m_pClient->m_GameInfo.m_aTeamSize[Team] > 32)
 	{
 		LineHeight = 10.0f;
 		TeeSizeMod = 0.4f;
@@ -468,7 +468,7 @@ float CScoreboard::RenderScoreboard(float x, float y, float w, int Team, const c
 			if(HighlightedLine)
 			{
 				CUIRect Rect = {x, y, w, LineHeight};
-				if(m_pClient->m_GameInfo.m_aTeamSize[Team] > 16)
+				if(m_pClient->m_GameInfo.m_aTeamSize[Team] > 32)
 					RenderTools()->DrawRoundRect(&Rect, vec4(1.0f, 1.0f, 1.0f, 0.75f*ColorAlpha), 3.0f);
 				else
 					RenderTools()->DrawRoundRect(&Rect, vec4(1.0f, 1.0f, 1.0f, 0.75f*ColorAlpha), 5.0f);