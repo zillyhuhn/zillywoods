e88fd0d6a14aa7a633e5d8b1d200a205c2ff6906
ChillerDragon <chillerdragon@gmail.com>
2020-07-26 12:05:50 +0200

    Fix skin changes break client recognition
    thanks @fokkonaut
    https://github.com/fokkonaut/F-Client/commit/0b158089370c8f21be195fedb770dcba8a9928d0

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -2016,8 +2016,6 @@ void CGameClient::SendSwitchTeam(int Team)
 void CGameClient::SendStartInfo(bool IsDummy)
 {
 	CNetMsg_Cl_StartInfo Msg;
-	char aClientStr[32];
-	str_format(aClientStr, sizeof(aClientStr), "zilly!%s", ZILLYWOODS_VERSION);
 	if(IsDummy)
 	{
 		Msg.m_pName = Config()->m_DummyName;
@@ -2025,7 +2023,7 @@ void CGameClient::SendStartInfo(bool IsDummy)
 		Msg.m_Country = Config()->m_DummyCountry;
 		for(int p = 0; p < NUM_SKINPARTS; p++)
 		{
-			Msg.m_apSkinPartNames[p] = m_pSkins->IsSkinPartDefault(IsDummy, p) ? aClientStr : CSkins::ms_apSkinVariables[IsDummy][p];
+			Msg.m_apSkinPartNames[p] = m_pSkins->IsSkinPartDefault(IsDummy, p) ? ZILLY_STRING : CSkins::ms_apSkinVariables[IsDummy][p];
 			Msg.m_aUseCustomColors[p] = *CSkins::ms_apUCCVariables[1][p];
 			Msg.m_aSkinPartColors[p] = *CSkins::ms_apColorVariables[1][p];
 		}
@@ -2040,7 +2038,7 @@ void CGameClient::SendStartInfo(bool IsDummy)
 		Msg.m_Country = Config()->m_PlayerCountry;
 		for(int p = 0; p < NUM_SKINPARTS; p++)
 		{
-			Msg.m_apSkinPartNames[p] = m_pSkins->IsSkinPartDefault(IsDummy, p) ? aClientStr : CSkins::ms_apSkinVariables[IsDummy][p];
+			Msg.m_apSkinPartNames[p] = m_pSkins->IsSkinPartDefault(IsDummy, p) ? ZILLY_STRING : CSkins::ms_apSkinVariables[IsDummy][p];
 			Msg.m_aUseCustomColors[p] = *CSkins::ms_apUCCVariables[0][p];
 			Msg.m_aSkinPartColors[p] = *CSkins::ms_apColorVariables[0][p];
 		}
@@ -2071,7 +2069,7 @@ void CGameClient::SendSkinChange(bool IsDummy)
 	CNetMsg_Cl_SkinChange Msg;
 	for(int p = 0; p < NUM_SKINPARTS; p++)
 	{
-		Msg.m_apSkinPartNames[p] = CSkins::ms_apSkinVariables[IsDummy][p];
+		Msg.m_apSkinPartNames[p] = m_pSkins->IsSkinPartDefault(IsDummy, p) ? ZILLY_STRING : CSkins::ms_apSkinVariables[IsDummy][p];
 		Msg.m_aUseCustomColors[p] = *CSkins::ms_apUCCVariables[IsDummy][p];
 		Msg.m_aSkinPartColors[p] = *CSkins::ms_apColorVariables[IsDummy][p];
 	}
